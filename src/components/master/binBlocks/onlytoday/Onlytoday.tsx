badce764849d8482627dbabb218eaa18f11847de
NULL <rgtntppy@gmail.com>
2025-05-11 17:48:20 +0900

    onlytodayを編集可能に変更

--- src/components/master/binBlocks/onlytoday/Onlytoday.tsx
+++ src/components/master/binBlocks/onlytoday/Onlytoday.tsx
@@ -1,4 +1,5 @@
 import './onlytoday.scss';
+import { useState, useRef } from 'react';
 import { OnlytodayProps } from './onlytodayInterface';
 import { BinDayBlock } from '../binDayBlocks/BinDayBlock';
 
@@ -12,10 +13,48 @@ export const Onlytoday: React.FC<OnlytodayProps> = ({
     onChange,
     onCheckboxToggle,
 }) => {
+    const [binName, setBinName] = useState(bin);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleBlur = () => {
+        setIsEditing(false);
+    };
+    
     return (
         <div className='onlytodayBinBlock'>
-            <div className='binName'>
-                {bin}
+            <div className='binNameWrapper'>
+                {isEditing ? (
+                    <input
+                        ref={inputRef}
+                        type='text'
+                        className='binNameInput'
+                        value={binName}
+                        maxLength={4}
+                        onChange={(e) => setBinName(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                e.currentTarget.blur();
+                            } 
+                        }}
+                        onBlur={handleBlur}
+                        autoFocus
+                    />
+                ) : (
+                    <p
+                        className='binNameText'
+                        onClick={() => {
+                            setIsEditing(true);
+                            setTimeout(() => inputRef.current?.focus(), 0);
+                        }}
+                    >
+                        {binName.length >= 3
+                            ? `${binName.slice(0, 2)}\n${binName.slice(2)}`
+                        : binName
+                        }
+                    </p>
+                )}
             </div>
             <BinDayBlock
                 label="当日分"
