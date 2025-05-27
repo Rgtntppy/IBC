1014323e6e32c1e1fcf5756ca562f2e414a212ea
NULL <rgtntppy@gmail.com>
2025-05-27 12:42:59 +0900

    user追加

--- src/data/users/users.ts
+++ src/data/users/users.ts
@@ -1,4 +1,5 @@
 export const users = [
     { id: 'IBC_user04', userName: 'IBC_user04', password: '808122', role: 'viewer', authority: 1 },
+    { id: '808228',     userName: 'IBC_user34', password: 'IBC_user34', role: 'viewer', authority: 1 },
     { id: '809617',     userName: 'IBC_user37', password: '809617', role: 'admin', authority: 9 },
 ];
